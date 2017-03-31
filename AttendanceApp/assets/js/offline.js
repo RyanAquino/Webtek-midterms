7b2fcc83a2babd929a47d9d1d747312021dc1df4
redd <reddpogi01@yahoo.com>
2017-03-31 20:56:45 +0800

    added local storage

--- AttendanceApp/assets/js/offline.js
+++ AttendanceApp/assets/js/offline.js
@@ -43,7 +43,86 @@ var info = document.getElementById("info");
 					};
 	localStorage.setItem("930", JSON.stringify(thirdSched));
 	var storedSecondSched = JSON.parse(localStorage.getItem("930"));
-
+//1030
+	var fourthSched ={ "time":"10:30-11:30",
+						"data":[
+						{"name":"Rhoderic Makil", "room":"s425", "absent":null, "date":null, "late":null},
+						{"name":"Maria Concepcion Clemente", "room":"s326", "absent":null, "date":null, "late":null},
+						{"name":"Jo Montes", "room":"s542", "absent":null, "date":null, "late":null}
+						]
+					};
+	localStorage.setItem("1030", JSON.stringify(fourthSched));
+	var storedFourthSched = JSON.parse(localStorage.getItem("1030"));
+//1130	
+	var fifthSched ={ "time":"11:30-12:30",
+						"data":[
+						{"name":"Rhoderic Makil", "room":"s425", "absent":null, "date":null, "late":null},
+						{"name":"Maria Concepcion Clemente", "room":"s326", "absent":null, "date":null, "late":null},
+						{"name":"Jo Montes", "room":"s542", "absent":null, "date":null, "late":null}
+						]
+					};
+	localStorage.setItem("1130", JSON.stringify(fifthSched));
+	var storedFifthSched = JSON.parse(localStorage.getItem("1130"));
+//1230
+	var sixthSched ={ "time":"12:30-1:30",
+						"data":[
+						{"name":"Rhoderic Makil", "room":"s425", "absent":null, "date":null, "late":null},
+						{"name":"Maria Concepcion Clemente", "room":"s326", "absent":null, "date":null, "late":null},
+						{"name":"Jo Montes", "room":"s542", "absent":null, "date":null, "late":null}
+						]
+					};
+	localStorage.setItem("1230", JSON.stringify(sixthSched));
+	var storedSixthSched = JSON.parse(localStorage.getItem("1230"));
+//130
+	var seventhSched ={ "time":"1:30-2:30",
+						"data":[
+						{"name":"Rhoderic Makil", "room":"s425", "absent":null, "date":null, "late":null},
+						{"name":"Maria Concepcion Clemente", "room":"s326", "absent":null, "date":null, "late":null},
+						{"name":"Jo Montes", "room":"s542", "absent":null, "date":null, "late":null}
+						]
+					};
+	localStorage.setItem("130", JSON.stringify(seventhSched));
+	var storedSeventhSched = JSON.parse(localStorage.getItem("130"));
+//230
+	var eigthSched ={ "time":"2:30-3:30",
+						"data":[
+						{"name":"Rhoderic Makil", "room":"s425", "absent":null, "date":null, "late":null},
+						{"name":"Maria Concepcion Clemente", "room":"s326", "absent":null, "date":null, "late":null},
+						{"name":"Jo Montes", "room":"s542", "absent":null, "date":null, "late":null}
+						]
+					};
+	localStorage.setItem("230", JSON.stringify(eigthSched));
+	var storedEigthSched = JSON.parse(localStorage.getItem("230"));
+//330
+	var ninthSched ={ "time":"3:30-4:30",
+						"data":[
+						{"name":"Rhoderic Makil", "room":"s425", "absent":null, "date":null, "late":null},
+						{"name":"Maria Concepcion Clemente", "room":"s326", "absent":null, "date":null, "late":null},
+						{"name":"Jo Montes", "room":"s542", "absent":null, "date":null, "late":null}
+						]
+					};
+	localStorage.setItem("330", JSON.stringify(ninthSched));
+	var storedNinthSched = JSON.parse(localStorage.getItem("330"));
+//430
+	var tenthSched ={ "time":"4:30-5:30",
+						"data":[
+						{"name":"Rhoderic Makil", "room":"s425", "absent":null, "date":null, "late":null},
+						{"name":"Maria Concepcion Clemente", "room":"s326", "absent":null, "date":null, "late":null},
+						{"name":"Jo Montes", "room":"s542", "absent":null, "date":null, "late":null}
+						]
+					};
+	localStorage.setItem("430", JSON.stringify(tenthSched));
+	var storedTenthSched = JSON.parse(localStorage.getItem("430"));
+//530
+	var eleventhSched ={ "time":"5:30-6:30",
+						"data":[
+						{"name":"Rhoderic Makil", "room":"s425", "absent":null, "date":null, "late":null},
+						{"name":"Maria Concepcion Clemente", "room":"s326", "absent":null, "date":null, "late":null},
+						{"name":"Jo Montes", "room":"s542", "absent":null, "date":null, "late":null}
+						]
+					};
+	localStorage.setItem("530", JSON.stringify(eleventhSched));
+	var storedSecondSched = JSON.parse(localStorage.getItem("530"));
 // END LOCAL STORAGE
 
 
@@ -90,6 +169,62 @@ function startUI(){
     li3.appendChild(litext3);
     ul.appendChild(li3);
   	li3.setAttribute("onclick", "nine()");
+  	// 1030==========================================================
+  	var litext4 = document.createTextNode(fourthSched.time);
+
+    var li4 =document.createElement("li");
+    li4.appendChild(litext4);
+    ul.appendChild(li4);
+  	li4.setAttribute("onclick", "ten()");
+  	// 1130==========================================================
+  	var litext5 = document.createTextNode(fifthSched.time);
+
+    var li5 =document.createElement("li");
+    li5.appendChild(litext5);
+    ul.appendChild(li5);
+  	li5.setAttribute("onclick", "eleven()");
+  	// 1230==========================================================
+  	var litext6 = document.createTextNode(sixthSched.time);
+
+    var li6 =document.createElement("li");
+    li6.appendChild(litext6);
+    ul.appendChild(li6);
+  	li6.setAttribute("onclick", "twelve()");
+  	// 130==========================================================
+  	var litext7 = document.createTextNode(seventhSched.time);
+
+    var li7 =document.createElement("li");
+    li7.appendChild(litext7);
+    ul.appendChild(li7);
+  	li7.setAttribute("onclick", "one()");
+  	// 230==========================================================
+  	var litext8 = document.createTextNode(eigthSched.time);
+
+    var li8 =document.createElement("li");
+    li8.appendChild(litext8);
+    ul.appendChild(li8);
+  	li8.setAttribute("onclick", "two()");
+  	// 330==========================================================
+  	var litext9 = document.createTextNode(ninthSched.time);
+
+    var li9 =document.createElement("li");
+    li9.appendChild(litext9);
+    ul.appendChild(li3);
+  	li9.setAttribute("onclick", "three()");
+  	// 430==========================================================
+  	var litext10 = document.createTextNode(tenthSched.time);
+
+    var li10 =document.createElement("li");
+    li10.appendChild(litext10);
+    ul.appendChild(li10);
+  	li10.setAttribute("onclick", "four()");
+  	// 530==========================================================
+  	var litext11 = document.createTextNode(eleventhSched.time);
+
+    var li11 =document.createElement("li");
+    li11.appendChild(litext11);
+    ul.appendChild(li11);
+  	li11.setAttribute("onclick", "nine()");
 }
 
 	
